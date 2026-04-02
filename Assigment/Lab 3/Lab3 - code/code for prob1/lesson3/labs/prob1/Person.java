d17d64e9b6eba9b2872c88b70ffda5ae5f9a2ee4
Phuong Nguyen <miu.nthphuong@gmail.com>
2026-04-02 22:08:16 +0700

    Lab 3 - Problem 1 - code
    Lab 3 - Problem 1 - code

--- Assigment/Lab 3/Lab3 - code/code for prob1/lesson3/labs/prob1/Person.java
+++ Assigment/Lab 3/Lab3 - code/code for prob1/lesson3/labs/prob1/Person.java
@@ -12,14 +12,15 @@ public class Person {
 	}
 	@Override
 	public boolean equals(Object aPerson) {
-		if(aPerson == null) return false; 
+		if(aPerson == null) return false;
 		if(!(aPerson instanceof Person)) return false;
 		Person p = (Person)aPerson;
 		boolean isEqual = this.name.equals(p.name);
 		return isEqual;
 	}
 	public static void main(String[] args) {
-		
+
 	}
+
 
 }
--- Assigment/Lab 3/Lab3 - code/code for prob1/lesson3/labs/prob1/PersonWithJob.java
+++ Assigment/Lab 3/Lab3 - code/code for prob1/lesson3/labs/prob1/PersonWithJob.java
@@ -1,33 +1,47 @@
 package lesson3.labs.prob1;
 
-public class PersonWithJob extends Person {
-	
+public class PersonWithJob {
+
 	private double salary;
-	
-	public double getSalary() {
-		return salary;
-	}
+
+	private Person person;
+
 	PersonWithJob(String n, double s) {
-		super(n);
+
+		person = new Person(n);
 		salary = s;
 	}
-	
+
+	public double getSalary() {
+
+		return salary;
+	}
+
+	public Person getPerson() {
+
+		return person;
+	}
+
 	@Override
 	public boolean equals(Object aPerson) {
-		if(aPerson == null) return false; 
-		if(!(aPerson instanceof PersonWithJob)) return false;
-		PersonWithJob p = (PersonWithJob)aPerson;
-		boolean isEqual = this.getName().equals(p.getName()) &&
-				this.getSalary()==p.getSalary();
+
+		if (aPerson == null)
+			return false;
+		if (!(aPerson instanceof PersonWithJob))
+			return false;
+		PersonWithJob p = (PersonWithJob) aPerson;
+		boolean isEqual = this.getPerson().getName().equals(p.getPerson().getName())
+				&& this.getSalary() == p.getSalary();
 		return isEqual;
 	}
+
 	public static void main(String[] args) {
-		Person p1 = new PersonWithJob("Joe", 30000);
-		Person p2 = new Person("Joe");
-		//As PersonsWithJobs, p1 should be equal to p2
+
+		PersonWithJob p1 = new PersonWithJob("Joe", 30000);
+		PersonWithJob p2 = new PersonWithJob("Joe", 30000);
+		// As PersonsWithJobs, p1 should be equal to p2
 		System.out.println("p1.equals(p2)? " + p1.equals(p2));
 		System.out.println("p2.equals(p1)? " + p2.equals(p1));
 	}
-
 
 }
